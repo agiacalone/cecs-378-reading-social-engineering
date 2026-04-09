3ce1966b8d8f7efbff8bd53d8acd25bd37509435
Anthony Giacalone <anthony.giacalone@csulb.edu>
2026-04-09 09:21:14 -0700

    remove old files

--- prog1.c
+++
@@ -1,4 +0,0 @@
-alert tcp $EXTERNAL_NET any -> $SQL_SERVERS $ORACLE_PORTS\      
-(msg: "ORACLE create database attempt:;\ 
-flow: to_server, established; content: "create database"; nocase;\      
-classtype: protocol-command-decode;)
